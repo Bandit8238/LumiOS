0bb332ec1065a4b287f91952c867ab3d47706a98
Bandit8238 <zech.sq@gmail.com>
2026-04-21 14:45:39 -0600

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
 import App from './App.tsx'
+
 import { KernelProvider } from './context/kernal/kernal.tsx'
 import { UserProvider } from './context/user/user.tsx'
 import WindowProvider from './context/window/WindowProvider.tsx'
